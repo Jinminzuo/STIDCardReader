c45ff05c11789ed3eedfadf48ac311cccbf54a30
Jinminzuo <110321926+Jinminzuo@users.noreply.github.com>
2025-09-13 09:38:04 +0800

    提交
    文件初始化提交

---
+++ MyFramework.xcframework' STIDCardReader-POD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFramework.xcframework' STIDCardReader-POD
+//
+//  Created by ios on 2025/9/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyFramework.xcframework' STIDCardReader-PODTests/MyFramework_xcframework__STIDCardReader_PODTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyFramework_xcframework__STIDCardReader_PODTests.swift
+//  MyFramework.xcframework' STIDCardReader-PODTests
+//
+//  Created by ios on 2025/9/13.
+//
+
+import Testing
+@testable import Pod__Spec_new_do__s____s_name___________MyFramework____s_version________1_0_0____s_summary________Binary_distribution_____s_description____Precompiled_xcframework_____s_homepage_______https___github_com_yourname_MyFramework____s_license__________type_____Commercial____file_____LICENSE______s_author___________Your_Name______you_example_com______s_source___________git_____https___github_com_yourname_MyFramework_git____tag____s_version_to_s______s_ios_deployment_target____12_0____s_vendored_frameworks____Binary_MyFramework_xcframework__STIDCardReader_POD
+
+struct MyFramework_xcframework__STIDCardReader_PODTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
